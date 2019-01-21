8aa1088f9b04d7177f5fef884d6bce7180e73762
caozilong <caozilong@allwinnertech.com>
2019-01-21 10:29:11 +0800

    upate
    Signed-off-by: caozilong <caozilong@allwinnertech.com>

--- luatest/hello.lua
+++ luatest/hello.lua
@@ -3,9 +3,11 @@ tbl = {name = "shun", id = 20114442}
 
 function add(a, b)
     --zilong.version()
-    avg, sum = average(0, 20, 30, 40, 50)  
-    return a + b, avg, sum  
+    --avg, sum = average(0, 20, 30, 40, 50)  
+    return 10  
 end
-print(zilong.version())
-print(zilong.success())
+--print(zilong.version())
+--print(zilong.success())
 --add(10, 2)
+
+return add
---
+++ luatest/string.lua
@@ -0,0 +1,8 @@
+--a="abcd"
+a = 1
+b = 2
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
