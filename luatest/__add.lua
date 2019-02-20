d28f75af79b8567cdc4ce835f8842df81d89883f
13824125580 <13824125580@163.com>
2019-02-21 07:34:44 +0800

    update
    Signed-off-by: 13824125580 <13824125580@163.com>

---
+++ luatest/__add.lua
@@ -0,0 +1,34 @@
+local mt = {}
+
+mt.__add = function(a,b)
+    coroutine.yield()
+    print("yield resume")
+    return a.v + b.v/2
+end
+
+local a = {v = 10}
+local b = {v = 12}
+
+setmetatable(a, mt)
+
+function traceback( msg )
+    print("----------------------------------------")
+    print("LUA ERROR: " .. tostring(msg) .. "\n")
+    print(debug.traceback())
+    print("----------------------------------------")
+end
+
+local function main()
+	print("hello")
+        print("a + b :", a + b)
+end
+
+
+coco = coroutine.create(function (a,b)
+    print("resume args:"..a..","..b)
+    ab, yr = xpcall(main, traceback)
+    print ("yreturn and ab:" ..yreturn, ab)
+end)
+coroutine.resume(coco,0,1)
+coroutine.resume(coco,21)
+
---
+++ luatest/addtest.lua
@@ -0,0 +1,5 @@
+function add(a, b)
+	return a + b
+end
+
+d = add(3, 4)
